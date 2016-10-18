cdcdab4a97a96110dc0fd0e74f8a68ea8539ae52
adetoito <adetoito2@gmail.com>
2016-10-18 14:19:52 -0400

    Second

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,20 +5,14 @@ public class Main {
     public static void main(String[] args) {
 
         /*
-
         indexOf is a String method that returns the index (position) of a letter
         from a certain index.
 
         public int indexOf(String s, int from)
 
         indexOf returns a public integer.
-        indexOf receives two parameters:
-
-        > a string (indicating what to find)
-        > an integer (indicating the starting index)
-
+        indexOf receives two parameters: a string and an integer.
          */
-
 
         // For example...
 
@@ -29,14 +23,6 @@ public class Main {
         The code finds the index of "e" from an index of 5 (before 'o').
         "8" is printed out because it is the index of the next "e".
          */
-
-        System.out.println(s.indexOf("e", 0));
-
-        /*
-        In this instance, "2" is printed out. This is because the code searches for the next
-        "e", starting at an index of 0 (the start of the string).
-         */
-
 
     }
 }
